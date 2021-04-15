34c70cbd198077e41f772e6862ac86a4f71ac14d
Aditya-011 <adihgr18@gmail.com>
2021-04-15 10:43:25 +0530

    15/04/2021

--- practice.py
+++ practice.py
@@ -1,1 +1,12 @@
-print(bin(10))
+text = ["The", "fox", "jumped", "over", "the", "fence", "."]
+
+
+def method(text):
+    result = ""
+    for i in text:
+        result = result+" " + i
+
+    return result
+
+
+print(method(text))
